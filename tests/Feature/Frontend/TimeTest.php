9f8c9c85215c62322ba95fadc1d30482fa7ed0d2
soringabriel <sorinmarica4@gmail.com>
2020-11-30 14:45:26 +0000

    Updates on unit testing

--- tests/Feature/Frontend/TimeTest.php
+++ tests/Feature/Frontend/TimeTest.php
@@ -6,17 +6,17 @@ use App\Domains\Auth\Models\User;
 use Tests\TestCase;
 
 /**
- * Class DashboardTest.
+ * Class TimeTest.
  */
-class DashboardTest extends TestCase
+class TimeTest extends TestCase
 {
     /** @test */
     public function only_authenticated_users_can_access_their_account()
     {
-        $this->get('/dashboard')->assertRedirect('/login');
+        $this->get('/time')->assertRedirect('/login');
 
         $this->actingAs(User::factory()->user()->create());
 
-        $this->get('/dashboard')->assertOk();
+        $this->get('/time')->assertOk();
     }
 }
