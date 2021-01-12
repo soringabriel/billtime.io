3bdce316de2516d443ce34802fb40e948e4cf5f6
soringabriel <sorinmarica4@gmail.com>
2021-01-12 16:37:33 +0000

    Fixes

--- app/Http/Requests/Frontend/Time/StoreTimeRequest.php
+++ app/Http/Requests/Frontend/Time/StoreTimeRequest.php
@@ -23,7 +23,7 @@ class StoreTimeRequest extends FormRequest
             'start_time' => ['required', 'date_format:Y-m-d H:i', 'before:' . Carbon::now()->timezone(auth()->user()->timezone), 'before:' . FormRequest::input('end_time'), 'after:' . Carbon::createFromFormat('Y-m-d H:i', FormRequest::input('end_time'))->subDay()],
             'end_time' => ['required', 'date_format:Y-m-d H:i', 'before:' . Carbon::now()->timezone(auth()->user()->timezone)],
             'project_id' => ['required', Rule::exists('projects', 'id')->where('user_id', auth()->user()->getParentId())],
-            'task' => ['max:255', 'url'],
+            'task' => ['max:255'],
             'details' => ['required', 'max:255'],
         ];
     }
--- app/Http/Requests/Frontend/Time/UpdateTimeRequest.php
+++ app/Http/Requests/Frontend/Time/UpdateTimeRequest.php
@@ -3,9 +3,9 @@
 namespace App\Http\Requests\Frontend\Time;
 
 use App\Models\Time;
-use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use Carbon\Carbon;
 
 /**
  * Class UpdateTimeRequest.
@@ -23,7 +23,7 @@ class UpdateTimeRequest extends FormRequest
             'start_time' => ['required', 'date_format:Y-m-d H:i', 'before:' . Carbon::now()->timezone(auth()->user()->timezone), 'before:' . FormRequest::input('end_time'), 'after:' . Carbon::createFromFormat('Y-m-d H:i', FormRequest::input('end_time'))->subDay()],
             'end_time' => ['required', 'date_format:Y-m-d H:i', 'before:' . Carbon::now()->timezone(auth()->user()->timezone)],
             'project_id' => ['required', Rule::exists('projects', 'id')->where('user_id', auth()->user()->getParentId())],
-            'task' => ['max:255', 'url'],
+            'task' => ['max:255'],
             'details' => ['required', 'max:255'],
         ];
     }
