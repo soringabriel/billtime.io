a6cca0d17528d31a5b21e6318e14865a8b005be7
soringabriel <sorinmarica4@gmail.com>
2021-01-12 20:21:18 +0000

    Fix unit tests

--- app/Http/Requests/Frontend/Time/StoreTimeRequest.php
+++ app/Http/Requests/Frontend/Time/StoreTimeRequest.php
@@ -20,7 +20,13 @@ class StoreTimeRequest extends FormRequest
     public function rules()
     {
         return [
-            'start_time' => ['required', 'date_format:Y-m-d H:i', 'before:' . Carbon::now()->timezone(auth()->user()->timezone), 'before:' . FormRequest::input('end_time'), 'after:' . Carbon::createFromFormat('Y-m-d H:i', FormRequest::input('end_time'))->subDay()],
+            'start_time' => [
+                'required', 
+                'date_format:Y-m-d H:i', 
+                'before:' . Carbon::now()->timezone(auth()->user()->timezone), 
+                'before:' . FormRequest::input('end_time'), 
+                (!empty(FormRequest::input('end_time')) ? 'after:' . Carbon::createFromFormat('Y-m-d H:i', FormRequest::input('end_time'))->subDay() : '')
+            ],
             'end_time' => ['required', 'date_format:Y-m-d H:i', 'before:' . Carbon::now()->timezone(auth()->user()->timezone)],
             'project_id' => ['required', Rule::exists('projects', 'id')->where('user_id', auth()->user()->getParentId())],
             'task' => ['max:255'],
