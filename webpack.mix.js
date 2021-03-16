775e9e6d10d3cffe342f56d0b3b2eef656dfe11e
soringabriel <sorinmarica4@gmail.com>
2021-03-16 06:48:14 +0000

    Presentation page theme integration

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,10 @@ const mix = require('laravel-mix');
 mix.setPublicPath('public')
     .setResourceRoot('../') // Turns assets paths in css relative to css file
     .sass('resources/sass/frontend/app.scss', 'css/frontend.css')
+    .sass('resources/sass/frontend/presentation.scss', 'css/presentation.css')
     .sass('resources/sass/backend/app.scss', 'css/backend.css')
     .js('resources/js/frontend/app.js', 'js/frontend.js')
+    .js('resources/js/frontend/presentation.js', 'js/presentation.js')
     .js('resources/js/backend/app.js', 'js/backend.js')
     .extract([
         'alpinejs',
