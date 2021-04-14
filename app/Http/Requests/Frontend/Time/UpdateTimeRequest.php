f6f0538ba66fc8503f1f7003683d81cfe08c5476
soringabriel <sorinmarica4@gmail.com>
2021-04-14 05:25:54 +0000

    2 minor fixes

--- app/Http/Requests/Frontend/Time/UpdateTimeRequest.php
+++ app/Http/Requests/Frontend/Time/UpdateTimeRequest.php
@@ -28,7 +28,7 @@ class UpdateTimeRequest extends FormRequest
                 (!empty(FormRequest::input('end_time')) ? 'after:' . Carbon::createFromFormat('Y-m-d H:i', FormRequest::input('end_time'))->subDay() : '')
             ],
             'end_time' => ['required', 'date_format:Y-m-d H:i', 'before_or_equal:' . Carbon::now()->timezone(auth()->user()->timezone)],
-            'project_id' => ['required', Rule::exists('projects', 'id')->where('user_id', auth()->user()->getParentId())],
+            'project_id' => ['required', Rule::exists('projects', 'id')->where('user_id', auth()->user()->getOrganizationOwnerId())],
             'task' => ['max:255'],
             'details' => ['required', 'max:255'],
         ];
