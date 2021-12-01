f96b9eab5f3fd937c4757066c87e2f70b40a8d8a
soringabriel <sorinmarica4@gmail.com>
2021-12-01 10:22:04 +0000

    Add test

--- tests/Feature/Frontend/Time/ListTimeTest.php
+++ tests/Feature/Frontend/Time/ListTimeTest.php
@@ -34,4 +34,24 @@ class ListTimeTest extends TestCase
 
         $this->get('/time')->assertOk();
     }
+
+    /** @test */
+    public function only_an_user_can_access_the_times_calendar_page()
+    {
+        $user = User::factory()->user()->create();
+        $organization = Organization::factory()->create(['owner_id' => $user->id]);
+        $user->update(['organization_id' => $organization->id]);
+
+        $this->get('/time/calendar')->assertRedirect('/login');
+
+        $this->actingAs($user);
+
+        $this->get('/time/calendar')->assertRedirect(route(homeRoute()));
+
+        $user->syncPermissions([
+            Permission::where('name', 'user.access.times.access')->first()->id, 
+        ]);
+
+        $this->get('/time/calendar')->assertOk();
+    }
 }
